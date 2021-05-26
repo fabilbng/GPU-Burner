44cd0467b11278c3ccdc1b2a2d87f32956e56b51
Fabian Liebing <fabian.liebing@buschmais.com>
2021-05-26 17:33:13 +0200

    added nbb sold out checker

--- addToCartScripts/nbb.js
+++ addToCartScripts/nbb.js
@@ -1,4 +1,9 @@
 const findNBBButton = () => {
+    const soldOutDiv = document.getElementsByClassName("soldOut")
+    if (soldOutDiv.length != 0){
+        notificationDiv.innerHTML = `<h3 style="color: white"> Product Sold Out</h3>`
+        return
+    }
     const addToCardButton = $(".shopping_cart_btn")
     if (addToCardButton.length !== 0){
         const productId = $("input[name='products_id']").attr('value')
