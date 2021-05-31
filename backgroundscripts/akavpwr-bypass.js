64c273bbe177d4c955d391545b420262b92587e2
Fabian Liebing <fabian.liebing@buschmais.com>
2021-05-31 15:01:19 +0200

    working on queuebp

--- backgroundscripts/akavpwr-bypass.js
+++ backgroundscripts/akavpwr-bypass.js
@@ -1,64 +1,75 @@
-const removeCookie =(cookie) => {
+const supportedDomains = ["www.notebooksbilliger.de/", "www.gamestop.de"]
+const removeCookie = (cookie) => {
     var url = "http" + (cookie.secure ? "s" : "") + "://" + cookie.domain +
         cookie.path;
-    console.log(url)
-    chrome.cookies.remove({url: url, name: cookie.name}, (result) => {
+    chrome.cookies.remove({url: url, name: cookie.name, storeId: cookie.storeId}, (result) => {
+        console.log('Removed')
         console.log(result)
     });
 }
 
-const setCookie = (cookie) => {
-    let newCookie = cookie
+const setCookie = async (cookie, newName) => {
+    console.log(cookie)
+    let newCookie = {...cookie}
     var url = "http" + (cookie.secure ? "s" : "") + "://" + cookie.domain
     delete newCookie['hostOnly']
     delete newCookie['session']
-    newCookie.name[5] = 'a'
-    newCookie.name[6] ='u'
+    delete newCookie['domain']
+    newCookie.name=newName
     chrome.cookies.set({...newCookie, url: url}, (result) => console.log(result))
 }
 
-const setAkavCookie = (site) => {
-    chrome.cookies.getAll({session: true, domain: site.url}, (cookies) => {
-        const akavCookie = cookies.find(cookie => cookie.name.includes('akavpwr'))
-        if(akavCookie){
-            setCookie(akavCookie)
-            return chrome.tabs.sendMessage(sender.tab.id, {message: 'addedAkavPauCookie'})
-        }
-        else {
-            return
-        }
-})}
+const setAkavCookie = (tabId, originUrl, options) => {
+    const domain = supportedDomains.find(domain => originUrl.includes(domain))
+    if (domain){
+        chrome.cookies.getAll({session: true, domain: domain}, (cookies) => {
+            const akavCookie = cookies.find(cookie => cookie.name.includes('akavpwr'))
+            if(akavCookie){
+                removeCookie(akavCookie)
+                setCookie(akavCookie, options.queueBpNewCookieName)
+                return chrome.tabs.sendMessage(tabId, {message: 'addedAkavPauCookie'})
+            }
+            else {
+                return
+            }
+        })
+    }
+    }
 
-const checkForBypassCookie = (site) => {
-    chrome.cookies.getAll({session: true, domain: site.url}, (cookies) => {
-        const akavCookie = cookies.find(cookie => cookie.name.includes('akavpwr'))
-        if(akavCookie){
-            return chrome.tabs.sendMessage(sender.tab.id, {message: 'createManualQueueBpButton', site: site})
-        }
-        else {
-            return
-        }
-})}
+const checkForBypassCookie = (tabId, originUrl) => {
+    const domain = supportedDomains.find(domain => originUrl.includes(domain))
+    if (domain){
+        chrome.cookies.getAll({session: true, domain: domain}, (cookies) => {
+            const akavCookie = cookies.find(cookie => cookie.name.includes('akavpwr'))
+            if(akavCookie){
+                    return chrome.tabs.sendMessage(tabId, {message: 'createManualQueueBpButton'})
+            }
+            else {
+                return
+            }
+        })
+    }
+    }
 
 
 chrome.runtime.onMessage.addListener(
     (request, sender) => {
-        if(request.message === 'checkForBypassCookie'){
-            getOptions().then(options => {
-                if (options.queueBpManualEnabled){
-                    checkForBypassCookie(request.site)
-                }
-                else if (options.queueBpAutomaticEnabled){
-                    setAkavCookie(request.site)
-                }
-                else {
-                    chrome.tabs.sendMessage(sender.tab.id, {message: 'QueueBypassDisabled'})
-                }
-            })
-        }
-        else if (request.message === 'startQueueBp'){
-            setAkavCookie(request.site)
-        }
+        getOptions().then(options => {
+            if(request.message === 'checkForBypassCookie'){
+                    if (options.queueBpManualEnabled){
+                        checkForBypassCookie(sender.tab.id, sender.origin)
+                    }
+                    else if (options.queueBpAutomaticEnabled){
+                        setAkavCookie(sender.tab.id, sender.origin, options)
+                    }
+                    else {
+                        chrome.tabs.sendMessage(sender.tab.id, {message: 'QueueBypassDisabled'})
+                    }
+            }
+            else if (request.message === 'startQueueBp'){
+                setAkavCookie(sender.tab.id, sender.origin, options)
+            }
+        })
     }
 )
 
--- globalContentScripts/akavpwr-bypass.js
+++ globalContentScripts/akavpwr-bypass.js
@@ -1,20 +1,15 @@
-const supportedSites= [{site:'notebooksbilliger', url: 'www.notebooksbilliger.de' }, {site:'gamestop', url:'www.gamestop.de' }]
 var notificationDivRight = document.createElement('div')
-
-const createNotificationDivRight = (notificationDivRight) => {
-    notificationDivRight.style.position = 'fixed'
-    notificationDivRight.style.bottom = '10px'
-    notificationDivRight.style.right = '10px'
-    notificationDivRight.style.width = '200px'
-    notificationDivRight.style.height = '65px'
-    notificationDivRight.style.backgroundColor = 'black'
-    notificationDivRight.style.opacity="100"
-    notificationDivRight.style.zIndex="9999"
-}
+notificationDivRight.style.position = 'fixed'
+notificationDivRight.style.bottom = '10px'
+notificationDivRight.style.right = '10px'
+notificationDivRight.style.width = '200px'
+notificationDivRight.style.height = '65px'
+notificationDivRight.style.backgroundColor = 'black'
+notificationDivRight.style.opacity="100"
+notificationDivRight.style.zIndex="9999"
 
 
-
-const createBypassButton = (notificationDivRight, site) => {
+const createBypassButton = (notificationDivRight, domain) => {
     notificationDivRight.innerHTML= '' +
         '<div style=" height: 100%; display: flex; flex-direction: column; align-items: center;">' +
         '<h3 style="margin-bottom:5px; color: white">Bypass Possible</h3>' +
@@ -22,28 +17,21 @@ const createBypassButton = (notificationDivRight, site) => {
         '</div>'
     const bypassButton = notificationDivRight.getElementsByClassName('bypassButton')
     bypassButton[0].addEventListener("click", () => {
-        chrome.runtime.sendMessage({message: 'startQueueBp', site: site})
+        chrome.runtime.sendMessage({message: 'startQueueBp'})
     }, false);
     document.body.appendChild(notificationDivRight)
 }
 
 const createSuccessNotificationDivRight = (notificationDivRight) => {
     notificationDivRight.innerHTML= '' +
-        '<div style=" height: 100%; display: flex; flex-direction: column; align-items: center;">' +
+        '<div style="height: 100%; display: flex; flex-direction: column; align-items: center;">' +
         '<h3 style="margin-bottom:5px; color: white">Added akavpau-cookie, refresh the page</h3>' +
         '</div>'
     document.body.appendChild(notificationDivRight)
 }
 
 const checkForCookie = () => {
-    supportedSites.some(
-        (site) => {
-            if (window.location.href.includes(site.site)){
-                createNotificationDivRight(notificationDivRight)
-                return chrome.runtime.sendMessage({message: 'checkForBypassCookie', site: site})
-            }
-        }
-    )
+        return chrome.runtime.sendMessage({message: 'checkForBypassCookie', site: window.location.href})
 }
 
 chrome.runtime.onMessage.addListener(
@@ -52,10 +40,10 @@ chrome.runtime.onMessage.addListener(
         createSuccessNotificationDivRight(notificationDivRight)
     }
     else if (request.message === 'createManualQueueBpButton'){
-        createBypassButton(notificationDivRight, request.site)
+        createBypassButton(notificationDivRight, request.domain)
     }
     else if (request.message === 'QueueBypassDisabled'){
-        notificationDivRight.innerHTML = 'QueueBp Disabled'
+        notificationDivRight.innerHTML = '<h3 style="color: white"> Queue bp disabled</h3>'
         document.body.appendChild(notificationDivRight)
     }
 })
